618964306b5f31c7c38000f725898f95161afea7
ds111g <ds111g@att.com>
2022-04-27 11:52:16 -0600

    add stepper.py, add scan()

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def scan():
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
-    parser.add_option("-s", "--scan", action="store", type="string", default="", dest="scan",
+    parser.add_option("-s", "--scan", action="store", type="string", default=None, dest="scan",
                       help="")
     parser.add_option("-c", "--center", action="store", type="int", default=0, dest="center",
                       help="")
