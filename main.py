638b7ebf86b9df33deca217493afd3847a2e47aa
ds111g <ds111g@att.com>
2022-04-29 16:32:05 -0600

    update timing

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option("-s", "--scan", action="store", type="string", default="true", dest="scan",
                       help="")
-    parser.add_option("-c", "--center", action="store", type="int", default=88, dest="center",
+    parser.add_option("-c", "--center", action="store", type="int", default=181, dest="center",
                       help="")
     parser.add_option("-v", "--height", action="store", type="int", default=270, dest="height",
                       help="")
